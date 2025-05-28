734aa22f97726e07c337c1e14125dc739dec5432
donnyfauzi <donnyfauzi55@gmail.com>
2025-05-28 19:11:44 +0700

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,8 @@ import video from '../assets/videos/video1.mp4';
 import bgImage1 from '../assets/images/hero/bg1.jpeg';
 import bgImage2 from '../assets/images/hero/bg2.png';
 import bgImage4 from '../assets/images/hero/bg-batik2.png';
-import bgImage5 from '../assets/images/hero/bg-map.jpg';
+import bgImage5 from '../assets/images/hero/foto-content2.jpg';
+import bgImage6 from '../assets/images/hero/foto-content3.jpg';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment } from '@fortawesome/free-solid-svg-icons';
@@ -86,7 +87,7 @@ export default function Home() {
         <div className="absolute top-0 inset-x-0 h-32 bg-gradient-to-b from-white/90 to-transparent z-10" />
 
         {/* Bottom Gradient */}
-        <div className="absolute bottom-0 inset-x-0 h-32 bg-gradient-to-t from-white/90 to-transparent z-10" />
+        <div className="absolute bottom-0 inset-x-0 h-32 bg-gradient-to-t from-white to-transparent z-10" />
 
         {/* Overlay */}
         <div className="absolute inset-0 z-0"></div>
@@ -105,7 +106,7 @@ export default function Home() {
               <>
                 <h2 className="text-3xl font-bold mb-6 text-center text-blue-500">APA ITU CODING ERLAS?</h2>
                 <div className="flex flex-col md:flex-row gap-6">
-                  <div className="w-full md:w-1/2 text-justify font-bold text-gray-700">
+                  <div className="w-full md:w-1/2 text-justify text-black">
                     <p className="mb-4">
                       Erlass Prokreatif Indonesia mengembangkan pembelajaran digital berbasis pemrograman, yaitu Coding Erlass. Coding Erlass merupakan pembelajaran pemrograman yang didukung oleh video pembelajaran.
                     </p>
@@ -113,7 +114,7 @@ export default function Home() {
                       Coding Erlass menyediakan berbagai video pembelajaran mengenai bahasa pemrograman Scratch. Mulai dari pengenalan fitur Scratch, memahami pergerakan Scratch, membuat animasi menarik, membuat game, hingga pembelajaran matematika sederhana.
                     </p>
                   </div>
-                  <div className="w-full md:w-1/2 text-justify font-bold text-gray-700">
+                  <div className="w-full md:w-1/2 text-justify text-black">
                     <p className="mb-4">
                       Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. InJourney Airports mengelola dan mengoperasikan 37 bandara dengan keunggulan layanan berkelas dunia dan keramahtamahan khas Indonesia.
                     </p>
@@ -134,10 +135,10 @@ export default function Home() {
               <>
                 <h2 className="text-3xl font-bold mb-6 text-center text-blue-500">KENAPA HARUS CODING DI ERLAS?</h2>
                 <div className="flex flex-col md:flex-row gap-6">
-                  <div className="w-full md:w-1/2 text-justify font-bold text-gray-700">
+                  <div className="w-full md:w-1/2 text-justify  text-black">
                     Karena Erlass menghadirkan kurikulum menyenangkan dan terstruktur untuk anak-anak Indonesia.
                   </div>
-                  <div className="w-full md:w-1/2 text-justify font-bold text-gray-700">
+                  <div className="w-full md:w-1/2 text-justify  text-black">
                     Video pembelajaran interaktif dan konten lokal menjadikan pengalaman belajar menyenangkan dan relevan.
                   </div>
                 </div>
@@ -148,7 +149,7 @@ export default function Home() {
             {currentSlide === 2 && (
               <>
                 <h2 className="text-3xl font-bold mb-6 text-center text-blue-500">Slide Tambahan</h2>
-                <div className="text-center font-semibold text-gray-600">
+                <div className="text-center  text-black">
                   Konten tambahan bisa ditambahkan di sini jika slide ke-3 aktif.
                 </div>
               </>
@@ -158,62 +159,79 @@ export default function Home() {
       </div>
 
       {/* Hero Konten 2 */}
-        <div className="w-full relative py-16 px-10 text-gray-800 overflow-hidden min-h-[500px]">
-          {/* Background kanan */}
-          <div
-            className="absolute inset-0 w-full h-full rounded-xl"
+      <div className="w-full relative py-16 px-10 text-gray-800 overflow-hidden min-h-[600px]">
+        {/* Background kanan */}
+        <div className="absolute inset-0 w-full h-full"
           style={{
-              backgroundImage: `url(${bgImage5})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              zIndex: 0,
-            }}
-          ></div>
-
-          {/* Overlay */}
-          <div className="absolute inset-0 bg-white/80 z-0"></div>
-
-          {/* Konten berdasarkan currentSlide */}
-          <AnimatePresence mode="wait">
-            <motion.div
-              
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: -100 }}
-              transition={{ duration: 0.5 }}
-              className="relative z-10"
-            >
-              <>
-                <h2 className="text-3xl font-bold mb-6 text-center text-blue-500">APA ITU CODING ERLAS?</h2>
-                <div className="flex flex-col md:flex-row gap-6">
-                  <div className="w-full md:w-1/2 text-justify font-bold text-gray-700">
-                    <p className="mb-4">
-                      Erlass Prokreatif Indonesia mengembangkan pembelajaran digital berbasis pemrograman, yaitu Coding Erlass. Coding Erlass merupakan pembelajaran pemrograman yang didukung oleh video pembelajaran.
-                    </p>
-                    <p className="mb-4">
-                      Coding Erlass menyediakan berbagai video pembelajaran mengenai bahasa pemrograman Scratch. Mulai dari pengenalan fitur Scratch, memahami pergerakan Scratch, membuat animasi menarik, membuat game, hingga pembelajaran matematika sederhana.
-                    </p>
-                  </div>
-                  <div className="w-full md:w-1/2 text-justify font-bold text-gray-700">
-                    <p className="mb-4">
-                      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. InJourney Airports mengelola dan mengoperasikan 37 bandara dengan keunggulan layanan berkelas dunia dan keramahtamahan khas Indonesia.
-                    </p>
-                    <p className="mb-4">
-                      Integer feugiat, arcu a egestas laoreet, turpis libero aliquet tortor, nec sollicitudin enim justo ac sem.
-                    </p>
-                  </div>
-                </div>
-              </>
-            </motion.div>
-          </AnimatePresence>
+            backgroundImage: `url(${bgImage5})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            zIndex: 0,
+          }}>
         </div>
+
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-white/20 z-0"></div>
+        {/* Top Gradient */}
+        <div className="absolute top-0 inset-x-0 h-32 bg-gradient-to-b from-white to-transparent z-10" />
+      
+
+        {/* Konten berdasarkan currentSlide */}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9, y: 50 }}
+          whileInView={{ opacity: 1, scale: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.3 }}
+          transition={{ duration: 2, ease: 'linear' }}
+          className="relative z-10"
+        >
+          <>
+          <h2 className="text-2xl font-bold mt-6 mb-6 text-center text-gray-700">Jembatan udara Indonesia, pintu gerbang menuju dunia.
+            InJourney Airports menghadirkan layanan standar dunia di<span className='text-4xl font-bold text-blue-600'> 20</span><span className='text-lg text-blue-600'> wilayah</span>
+          </h2>
+          <h2 className="text-2xl font-bold mb-6 text-center text-gray-700">InJourney Airports mengukuhkan posisinya sebagai perusahaan bandara
+            terbesar ke<span className='text-4xl font-bold text-blue-600'> 20</span><span className='text-lg text-blue-600'> di indonesia</span>
+          </h2>
+          </>
+        </motion.div>
+      </div>
+
+      {/* Hero Konten 3 */}
+      <div className="w-full relative py-16 px-10 overflow-hidden min-h-[300]">
+        <div className="absolute inset-0 w-full h-full"
+          style={{
+            backgroundImage: `url(${bgImage6})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            zIndex: 0,
+          }}>
+        </div>
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9, y: 50 }}
+          whileInView={{ opacity: 1, scale: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.3 }}
+          transition={{ duration: 2, ease: 'linear' }}
+          className="relative z-10"
+        >
+          <>
+          <h2 className="text-2xl font-bold mt-6 mb-6 text-center text-gray-700">Jembatan udara Indonesia, pintu gerbang menuju dunia.
+            InJourney Airports menghadirkan layanan standar dunia di<span className='text-4xl font-bold text-blue-600'> 20</span><span className='text-lg text-blue-600'> wilayah</span>
+          </h2>
+          <h2 className="text-2xl font-bold mb-6 text-center text-gray-700">InJourney Airports mengukuhkan posisinya sebagai perusahaan bandara
+            terbesar ke<span className='text-4xl font-bold text-blue-600'> 20</span><span className='text-lg text-blue-600'> di indonesia</span>
+          </h2>
+          </>
+        </motion.div>
+      </div>
+      
+
 
       {/* Contact Us Floating Button */}
       <a
         href="#kontak"
-        className="fixed bottom-0 right-2 bg-blue-800 text-white px-4 py-2 rounded-md shadow-lg hover:bg-blue-700 transition-all z-50 flex items-center gap-2"
-      >
+        className="fixed bottom-0 right-2 bg-blue-800 text-white px-4 py-2 rounded-md shadow-lg hover:bg-blue-700 transition-all z-50 flex items-center gap-2">
         <FontAwesomeIcon icon={faComment} />
         Contact Us
       </a>
